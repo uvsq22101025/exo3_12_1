d067726f94f2ccc9a6e507f4f4ce120203c90b83
uvsq22101025 <florian.zinopoulos-yannopoulos@ens.uvsq.fr>
2021-10-01 14:32:23 +0200

    Réorganisation

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+	public static void main(String[] args) {
+		/* Première version de la classe.
+		ChaineCryptee crypt = new ChaineCryptee("Ceci est un test", 3);
+		System.out.println(crypt.decrypte());
+		System.out.println(crypt.crypte());
+		
+		
+		ChaineCryptee testNull = new ChaineCryptee(null, 3);
+		System.out.println(testNull.decrypte());
+		System.out.println(testNull.crypte());
+		*/
+		
+		/* Test de ChaineCryptee2
+		ChaineCryptee2 crypt = new ChaineCryptee2("Ceci est un test", 3);
+		System.out.println(crypt.decrypte());
+		
+		
+		ChaineCryptee testNull = new ChaineCryptee(null, 3);
+		System.out.println(testNull.decrypte());
+		*/
+	}
+
+}
